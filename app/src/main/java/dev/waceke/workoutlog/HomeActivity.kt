f2913c7c2873cb5e597bb390548f564bfbaf473f
Grace-Waceke <gracewacekemwangi@gmail.com>
2022-06-18 09:00:25 +0300

    implemented planfragments

--- app/src/main/java/dev/waceke/workoutlog/HomeActivity.kt
+++ app/src/main/java/dev/waceke/workoutlog/HomeActivity.kt
@@ -2,10 +2,47 @@ package dev.waceke.workoutlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var fcvHome: FragmentContainerView
+    lateinit var bnvHome: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        castViews()
+        setupBottomNav()
+    }
+
+    fun castViews() {
+        fcvHome = findViewById(R.id.fcvHome)
+        bnvHome = findViewById(R.id.bnvHome)
+    }
+
+    fun setupBottomNav() {
+        bnvHome.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.Plan -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.fcvHome, PlanFragment())
+                    transaction.commit()
+                    true
+                }
+                R.id.Track -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.fcvHome, PlanFragment())
+                    transaction.commit()
+                    true
+                }
+                R.id.Profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fcvHome, ProfileFragment()).commit()
+                    true
+                }
+                else -> false
+
+            }
+        }
     }
 }
